b503c79370b896ed16c1f5e3c26cffa3f8d440b4
K.Yoshida <yoshida@cosmo-w.co.jp>
2026-06-01 00:40:30 +0900

    じゃんけんアプリの画面レイアウト作成途中

--- src/main/java/com/example/demo/jankenController.java
+++ src/main/java/com/example/demo/jankenController.java
@@ -1,14 +1,26 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class jankenController {
+public class JankenController {
 	
+	//初期表示
 	@GetMapping("/")
 	public String index() {
 		return "index";
 	}
+	
+	//画面から選択された手を受け取り、Modelに詰めて画面へ渡す
+	@PostMapping("/play")
+	public String play(@RequestParam("hand") String hand, Model model) {
+		model.addAttribute("hand", hand);
+		
+		return "index";
+	}
 }
+
