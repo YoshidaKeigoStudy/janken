c1c09183f5a59613ff47294eb7280188c440e7bc
K.Yoshida <yoshida@cosmo-w.co.jp>
2026-06-04 00:44:32 +0900

    勝敗判定サービスクラスの追加、CPUの手のランダム生成機能追加

--- src/main/java/com/example/demo/controller/JankenController.java
+++ src/main/java/com/example/demo/controller/JankenController.java
@@ -8,13 +8,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.service.JankenService;
+import com.example.demo.service.WinLoseJudgeService;
 
 @Controller
 public class JankenController {
-	//じゃんけんサービスクラス型の変数定義
-	//JankenService jankenService = new JankenService();
-	@Autowired
+	//じゃんけんサービスクラス型の変数を定義
+	@Autowired//DI注入アノテーション
 	private JankenService jankenService;
+	@Autowired
+	private WinLoseJudgeService winLoseJudgeService;
 	
 	//初期表示
 	@GetMapping("/")
@@ -25,10 +27,19 @@ public class JankenController {
 	//画面から選択された手を受け取り、Modelに詰めて画面へ渡す
 	@PostMapping("/play")
 	public String play(@RequestParam("hand") String hand, Model model) {
-		String cpuHand;
+		/** 変数宣言 **/
+		String cpuHand;		//CPUの手
+		String judge;		//勝敗判定
+		
+		/** 値の注入 **/
+		cpuHand = jankenService.cpuHand();	//jankenServiceクラスのcpuHandメソッドからCPUの手を取得
+		judge = winLoseJudgeService.winLoseJudge(hand, cpuHand);		//勝敗判定の値取得
+		
+		//画面に渡す値を詰める
 		model.addAttribute("hand", hand);
-		cpuHand = jankenService.cpuHand();
 		model.addAttribute("cpuHand", cpuHand);
+		model.addAttribute("judgeResult", judge);
+		
 		return "index";
 	}
 }
--- src/main/java/com/example/demo/service/JankenService.java
+++ src/main/java/com/example/demo/service/JankenService.java
@@ -1,11 +1,35 @@
 package com.example.demo.service;
 import org.springframework.stereotype.Service;
+import java.util.Random;
 
 @Service
 public class JankenService {
-	public String cpuHand;
 	public String cpuHand() {
-		this.cpuHand = "CPUの手はグーです";
+		/** 変数宣言 **/
+		Random rand = new Random();
+		int handNum = rand.nextInt(3);
+		String cpuHand = null;
+		
+		/** メソッド呼び出し **/
+		cpuHand = setCpuHand(handNum);
+		
+		return cpuHand;
+	}
+	
+	//乱数からCPUの手を生成するメソッド
+	public String setCpuHand(int handNum) {
+		String cpuHand = null;
+		switch(handNum) {
+		case 0:
+			cpuHand = "グー";
+			break;
+		case 1:
+			cpuHand = "チョキ";
+			break;
+		case 2:
+			cpuHand = "パー";
+			break;
+		}
 		return cpuHand;
 	}
 }
